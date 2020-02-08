c37fb8bcd1aacaf72aaba5e2818f32c9ab8d769d
Fran4ssisi <carloes87@gmail.com>
2020-02-08 12:56:41 +0900

    영속성 컨텍스트(준영속상태)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,58 +15,21 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //flush insert문이 호출된다.료
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            //엔티티를 생성한 상태(비영속)
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
+            //특정 엔티티만 준영속 상태로 전환
+//            em.detach(member);료
 
-//            System.out.println("=== BEFORE ===");
-            //엔티티를 영속
-            //1차 캐시에 저장됨
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
+            //영속성 컨텍스트를 완전히 초기화
+            em.clear();
+            Member member2 = em.find(Member.class, 150L);
 
-            //1차 캐시에서 조회
-//            Member findMember = em.find(Member.class, 101L);
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName());
-
-            //영속성에서 제외시킨다(준영속)
-//            em.detach(member);
-            //영속삭제
-//            em.remove(member);
-
-
-            //1차캐시
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L); //1차캐시에서 가져온다
-//
-//            System.out.println("result = " + (findMember1 == findMember2));
-
-
-
-            //persist의 흐름 이해
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-//            System.out.println("=============================");
-
-
-//            Member member = em.find(Member.class, 150L);
-//            member.setName("ZZZZZ");
-//            em.persist(member); //할 필요없다 자바 컬렉션과 같다
-
-
-            //flush insert문이 호출된다.
-            Member member = new Member(200L, "member200");
-            em.persist(member);
-            em.flush();
+            //영속성 컨텍스트를 종
+//            em.close();
 
             System.out.println("=============================");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
